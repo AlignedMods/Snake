65a94e2e7655008cc52bca5a956e0cf094a6de55
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-31 16:33:15 +0200

    window created

--- main.cpp
+++ main.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 #include <raylib.h>
 
-using namespace std;
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
 
-int main () {
+int cellSize = 30;
+int cellCount = 25;
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+int main() {
 
-    cout << "Hello World" << endl;
+    const int width = 750;
+    const int height = 750;
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "game");
     SetTargetFPS(60);
 
-    while (WindowShouldClose() == false){
+    while (WindowShouldClose() == !true) 
+    {
         BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
 
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
+        ClearBackground(green);
 
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
         EndDrawing();
     }
+    
 
     CloseWindow();
     return 0;
