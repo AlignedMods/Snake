2639843a165eda3be3e742593f20d2bdeb31d6f0
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-31 16:40:53 +0200

    food draws

--- main.cpp
+++ main.cpp
@@ -7,19 +7,31 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
-int main() {
+class Food {
 
-    const int width = 750;
-    const int height = 750;
+    public:
+    Vector2 position = {5,6};
+
+    void Draw() 
+    {
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
+
+};
+
+int main() {
 
     InitWindow(cellSize * cellCount, cellSize * cellCount, "game");
     SetTargetFPS(60);
+
+    Food food = Food();
 
     while (WindowShouldClose() == !true) 
     {
         BeginDrawing();
 
         ClearBackground(green);
+        food.Draw();
 
         EndDrawing();
     }
