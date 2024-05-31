8c64f449b51b38ccfef28cea17d4f74c59e62e24
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-31 18:35:43 +0200

    drawing the snake

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
 int cellSize = 30;
 int cellCount = 25;
+
+class Snake
+{
+    public:
+    std::deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+    void Draw()
+    {
+        for (unsigned int i = 0; i < body.size(); i++)
+        {
+            float x = body[i].x;
+            float y = body[i].y;
+
+            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
+
+            DrawRectangleRounded(segment,0.6, 6, darkGreen);
+        }
+    }
+};
 
 class Food {
 
@@ -47,6 +67,7 @@ int main() {
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
 
     while (WindowShouldClose() == !true) 
     {
@@ -54,6 +75,7 @@ int main() {
 
         ClearBackground(green);
         food.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
