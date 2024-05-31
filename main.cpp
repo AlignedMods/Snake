c747c6901de60d130ae0fb034ccedbcee3360249
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-31 17:04:11 +0200

    food now draws in different spots (also has a texture)

--- main.cpp
+++ main.cpp
@@ -10,11 +10,33 @@ int cellCount = 25;
 class Food {
 
     public:
-    Vector2 position = {5,6};
+    Vector2 position;
+    Texture2D texture;
+
+    Food()
+    {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = generateRandomPos();
+    }
+
+    ~Food() 
+    {
+        UnloadTexture(texture);
+    }
 
     void Draw() 
     {
-        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+
+    Vector2 generateRandomPos()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+
+        return Vector2{x, y};
     }
 
 };
